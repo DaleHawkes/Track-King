64b8ecb98e495a26760d52ec52d85736bc4e4e11
DaleHawkes <dalenoworries@gmail.com>
2015-01-02 07:54:23 +0000

    More changes to apply the Bootstrap template

--- Includes/Navigation.php
+++ Includes/Navigation.php
@@ -9,13 +9,13 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="http://127.0.0.1/my%20portable%20files/Track-King/index.php" title='Track King Home!'>Home</a>
+            <a class="navbar-brand" href="index.php" title='Home'>Home</a>
         </div>
  
         <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
                 <li>
-                    <a href="#">Single Race Analysis</a>
+                    <a href="analysis.php" title='Single Race Analysis'>Single Race Analysis</a>
                 </li>
                 <li>
                     <a href="#">Single Race Polar Diagram</a>
--- single_race_polar_diagram.php
+++ single_race_polar_diagram.php
@@ -4,49 +4,21 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width-device-width, initial-scale=1" />
 
-<title>Single Race Polar Diagram In Tables</title>
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+<title>Track King Index</title>
+<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 <link href="css/style.css" rel="stylesheet">
 
 </head>
 
 <body>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+<script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 
-<!-- THIS IS THE START OF THE NAVIGATION BAR -->
-<div class="navbar navbar-default navbar-fixed-top" role="navigation">
-    <div class="container">
- 
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="http://127.0.0.1/my%20portable%20files/Track-King/index.php" title='Track King Home!'>Home</a>
-        </div>
- 
-        <div class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-                <li class="active">
-                    <a href="#">Link One</a>
-                </li>
-                <li>
-                    <a href="#">Link Two</a>
-                </li>
-                <li>
-                    <a href="#">Link Three</a>
-                </li>
-            </ul>
-        </div><!--/.nav-collapse -->
- 
-    </div>
-</div>
-<!-- THIS IS THE END OF THE NAVIGATION BAR -->
+<!-- Site Navigation -->
+<?php include 'Includes/Navigation.php'; ?>
 
-<div class="container"> <!-- MAIN CONTENT AREA -->
+<div class="container"> 
+<!-- START OF MAIN CONTENT AREA -->
 
 <!-- Site banner -->       	
     <div class="banner">
@@ -218,50 +190,17 @@ echo "<tr><td>".$point."</td><td>".$latitude."</td><td>".$longitude."</td><td>".
 <br />
 <br />
 
-<!-- BACK TO TOP -->
-<a href="#" class="back-to-top">Back to Top</a>
-
-       <script>            
-			jQuery(document).ready(function() {
-				var offset = 220;
-				var duration = 500;
-				jQuery(window).scroll(function() {
-					if (jQuery(this).scrollTop() > offset) {
-						jQuery('.back-to-top').fadeIn(duration);
-					} else {
-						jQuery('.back-to-top').fadeOut(duration);
-					}
-				});
-				
-				jQuery('.back-to-top').click(function(event) {
-					event.preventDefault();
-					jQuery('html, body').animate({scrollTop: 0}, duration);
-					return false;
-				})
-			});
-		</script>
+<!-- Back To Top -->
+<?php include 'Includes/Back-To-Top.php'; ?>
 
 <br />
 <br />
+
 <!-- Site footer -->
-    <div class="bottom">
-        <div class="container">
-            <div class="col-md-4">
-                <h3><span class="glyphicon glyphicon-heart"></span> Footer section 1</h3>
-                <p>Content for the first footer section.</p>
-            </div>
-            <div class="col-md-4">
-                <h3><span class="glyphicon glyphicon-star"></span> Footer section 2</h3>
-                <p>Content for the second footer section.</p>
-            </div>
-            <div class="col-md-4">
-                <h3><span class="glyphicon glyphicon-music"></span> Footer section 3</h3>
-                <p>Content for the third footer section.</p>
-            </div>
-        </div>
-    </div>
-        
-</div> <!-- END OF MAIN CONTENT AREA -->
+<?php include 'Includes/Footer.php'; ?>
+      
+</div> 
+<!-- END OF MAIN CONTENT AREA -->
 
 </body>
 </html>
