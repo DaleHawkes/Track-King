f6dde9afcfc5681706cd0e28c1623ffd5a35a53a
DaleHawkes <dalenoworries@gmail.com>
2015-01-02 08:45:41 +0000

    Changed a link in navigation

--- Includes/Navigation.php
+++ Includes/Navigation.php
@@ -9,7 +9,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.php" title='Track King Home!'>Home</a>
+            <a class="navbar-brand" href="index.php" title='Home'>Home</a>
         </div>
  
         <div class="navbar-collapse collapse">
