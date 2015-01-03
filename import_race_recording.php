483130004c2fb122a477d2408fd8ba37631382aa
DaleHawkes <dalenoworries@gmail.com>
2015-01-03 09:08:51 +0000

    Started new upload form

---
+++ import_race_recording.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width-device-width, initial-scale=1" />
+
+<title>Track King Index</title>
+<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+<link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+<script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+<!-- Site Navigation -->
+<?php include 'Includes/Navigation.php'; ?>
+
+<div class="container"> 
+<!-- START OF MAIN CONTENT AREA -->
+
+<!-- Site banner -->       	
+    <div class="banner">
+        <div class="container">
+ 
+            <h1>Track King</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+    </div>
+<br />
+
+<!-- ENTER OUR CONTENT HERE -->
+
+
+
+<!-- END OF CONTENT HERE -->
+
+<br />
+<br />
+
+<!-- Back To Top -->
+<?php include 'Includes/Back-To-Top.php'; ?>
+
+<br />
+<br />
+
+<!-- Site footer -->
+<?php include 'Includes/Footer.php'; ?>
+      
+</div> 
+<!-- END OF MAIN CONTENT AREA -->
+
+</body>
+</html>
