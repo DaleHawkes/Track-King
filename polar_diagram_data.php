31d1095deaf80a57603ad07516dac1f1909e1e7f
DaleHawkes <dalenoworries@gmail.com>
2015-02-21 13:35:09 +0000

    Polar Diagram is now slightly working, bearings variable is not counting correctly

--- polar_diagram_data.php
+++ polar_diagram_data.php
@@ -62,7 +62,7 @@ for ($for_loop_counter = 0; $for_loop_counter <= 360; $for_loop_counter++)
 {
 
 		//$sql = "SELECT * FROM race_recording, race_information WHERE WindSpeedStart = '8 to 10' AND WindSpeedEnd = '8 to 10' AND Bearing = '$for_loop_counter'";
-        $sql = "SELECT race_information.*, race_recording.* FROM race_information LEFT JOIN race_recording ON race_information.date LIKE race_recording.time WHERE WindSpeedStart = '8 to 10' AND WindSpeedEnd = '8 to 10' AND Bearing = '$for_loop_counter'";
+        $sql = "SELECT race_information.*, race_recording.* FROM race_information LEFT JOIN race_recording ON race_information.date = race_recording.date WHERE WindSpeedStart = '8 to 10' AND WindSpeedEnd = '8 to 10' AND Bearing = '$for_loop_counter'";
         //$sql = "SELECT * FROM race_recording WHERE Time LIKE '%2014-11-23%' AND Bearing = '$for_loop_counter'";
 		$result = mysql_query($sql)or die(mysql_error());
 
@@ -98,6 +98,7 @@ for ($for_loop_counter = 0; $for_loop_counter <= 360; $for_loop_counter++)
   $longitude = $row['Longitude'];
   $bearing   = $row['Bearing'];
   $speed     = $row['Speed'];
+  $date      = $row['Date'];
   $time      = $row['Time'];
   $WindSpeedStart     = $row['WindSpeedStart'];
   $WindSpeedEnd      = $row['WindSpeedEnd'];
@@ -107,11 +108,11 @@ for ($for_loop_counter = 0; $for_loop_counter <= 360; $for_loop_counter++)
   // Modify ActualSpeed to two decimal places
   $actualspeeddecimal = number_format($actualspeed, 2, '.', '');
   
-  $date_time = $time;
-  list($splitdate, $splittime) = explode('T', $date_time); //We split the first part of the string with the letter T
+  //$date_time = $time;
+  //list($splitdate, $splittime) = explode('T', $date_time); //We split the first part of the string with the letter T
   
   // Using ASCII 46 we can remove the full stops from the time variable
-  $splittime = str_replace(chr(46), '', $splittime);
+  //$splittime = str_replace(chr(46), '', $splittime);
   
   // Lets now work out the actual bearing. What point of sail are we on?
   
@@ -177,7 +178,7 @@ for ($for_loop_counter = 0; $for_loop_counter <= 360; $for_loop_counter++)
   
   // Now for each looped row
      
-echo "<tr><td>".$point."</td><td>".$latitude."</td><td>".$longitude."</td><td>".$WindSpeedStart."</td><td>".$WindSpeedEnd."</td><td>".$bearing."</td><td>".$actualspeeddecimal."</td><td>".$splitdate."</td><td>".$splittime."</td><td>".$true_bearing."</td><td>".$pointofsail."</td></tr>";
+echo "<tr><td>".$point."</td><td>".$latitude."</td><td>".$longitude."</td><td>".$WindSpeedStart."</td><td>".$WindSpeedEnd."</td><td>".$bearing."</td><td>".$actualspeeddecimal."</td><td>".$date."</td><td>".$time."</td><td>".$true_bearing."</td><td>".$pointofsail."</td></tr>";
 
 ## WE NEED TO USE THIS SPACE TO ADD UP OUR SPEED AND THEN DEVIDE BY THE TOTAL NUMBER OF ROWS ###################################################################################################################
 
